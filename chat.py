0b39009e57ebb62ef5c19de0acd1ea9bd0daf7e4
victor297 <davidvictor297@gmail.com>
2024-08-10 09:57:42 +0100

    chat box

--- chat.py
+++ chat.py
@@ -1,41 +1,145 @@
-import streamlit as st
-from langchain_google_genai import  ChatGoogleGenerativeAI
-from dotenv import load_dotenv
+import time
 import os
+import joblib
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
 
-# Set up Google Generative AI
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_API_KEY")
+# Initialize new chat session
+new_chat_id = f'{time.time()}'
+MODEL_ROLE = 'ai'
+AI_AVATAR_ICON = '✨'
 
-# Function to send a message to Gemini and get a response
+# Create a data/ folder if it doesn't already exist
+try:
+    os.mkdir('data/')
+except FileExistsError:
+    # data/ folder already exists
+    pass
 
-def get_gemini_response(user_message_ai_input):
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
-    response = model.predict(user_message_ai_input)
-    return response
+# Load past chats (if available)
+try:
+    past_chats: dict = joblib.load('data/past_chats_list')
+except FileNotFoundError:
+    past_chats = {}
 
+# Sidebar allows a list of past chats
+with st.sidebar:
+    st.write('# Past Chats')
+    if st.session_state.get('chat_id') is None:
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id] + list(past_chats.keys()),
+            format_func=lambda x: past_chats.get(x, 'New Chat'),
+            placeholder='_',
+        )
+    else:
+        st.session_state.chat_id = st.selectbox(
+            label='Pick a past chat',
+            options=[new_chat_id, st.session_state.chat_id] + list(past_chats.keys()),
+            index=1,
+            format_func=lambda x: past_chats.get(x, 'New Chat' if x != st.session_state.chat_id else st.session_state.chat_title),
+            placeholder='_',
+        )
+    # Save new chats after a message has been sent to AI
+    st.session_state.chat_title = f'ChatSession-{st.session_state.chat_id}'
 
-# Streamlit app layout
-st.write("Enter your message to translate it 📖📕!")
+st.write('# Chat with Gemini')
 
-# Input form for user message
-with st.form(key="chat_form"):
-    user_message = st.text_input("Your message:", "")
-    user_message_ai_input= ""
-    submit_button = st.form_submit_button(label="Send")
+# Chat history (allows to ask multiple questions)
+try:
+    st.session_state.messages = joblib.load(
+        f'data/{st.session_state.chat_id}-st_messages'
+    )
+    st.session_state.gemini_history = joblib.load(
+        f'data/{st.session_state.chat_id}-gemini_messages'
+    )
+    print('old cache')
+except FileNotFoundError:
+    st.session_state.messages = []
+    st.session_state.gemini_history = []
+    print('new_cache made')
 
-# Display the chat log
-if "chat_log" not in st.session_state:
-    st.session_state["chat_log"] = []
+# Initialize the AI model
+st.session_state.model = genai.GenerativeModel('gemini-pro')
+st.session_state.chat = st.session_state.model.start_chat(
+    history=st.session_state.gemini_history,
+)
 
-# Handle form submissions
-if submit_button and user_message_ai_input:
-    response = get_gemini_response(user_message_ai_input)
-    st.session_state["chat_log"].append(f"You: {user_message}")
-    st.session_state["chat_log"].append(f"Translated to: {response}")
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(
+        name=message['role'],
+        avatar=message.get('avatar'),
+    ):
+        st.markdown(message['content'])
 
-# Display the chat log
-for chat in st.session_state["chat_log"]:
-    st.write(chat)
+# React to user input
+if prompt := st.chat_input('Your message here...'):
+    # Customize the user message with a mental wellness support introduction
+    custom_prompt = f"I need assistance for mental wellness. Help me, you will be my therapist, so here is my question: {prompt}"
+    
+    # Save this as a chat for later
+    if st.session_state.chat_id not in past_chats.keys():
+        past_chats[st.session_state.chat_id] = st.session_state.chat_title
+        joblib.dump(past_chats, 'data/past_chats_list')
+    
+    # Display user message in chat message container
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    
+    # Add user message to chat history
+    st.session_state.messages.append(
+        dict(
+            role='user',
+            content=prompt,
+        )
+    )
+    
+    ## Send customized message to AI
+    response = st.session_state.chat.send_message(
+        custom_prompt,
+        stream=True,
+    )
+    
+    # Display assistant response in chat message container
+    with st.chat_message(
+        name=MODEL_ROLE,
+        avatar=AI_AVATAR_ICON,
+    ):
+        message_placeholder = st.empty()
+        full_response = ''
+        # Streams in a chunk at a time
+        for chunk in response:
+            # Simulate stream of chunk
+            for ch in chunk.text.split(' '):
+                full_response += ch + ' '
+                time.sleep(0.05)
+                # Rewrites with a cursor at end
+                message_placeholder.write(full_response + '▌')
+        # Write full message with placeholder
+        message_placeholder.write(full_response)
+
+    # Add assistant response to chat history
+    st.session_state.messages.append(
+        dict(
+            role=MODEL_ROLE,
+            content=st.session_state.chat.history[-1].parts[0].text,
+            avatar=AI_AVATAR_ICON,
+        )
+    )
+    st.session_state.gemini_history = st.session_state.chat.history
+    # Save to file
+    joblib.dump(
+        st.session_state.messages,
+        f'data/{st.session_state.chat_id}-st_messages',
+    )
+    joblib.dump(
+        st.session_state.gemini_history,
+        f'data/{st.session_state.chat_id}-gemini_messages',
+    )
